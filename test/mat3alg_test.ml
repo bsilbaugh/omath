3dc0948305ab52cc2fe92ac86095fb36cb5379af
Benjamin Silbaugh <ben.silbaugh@gmail.com>
2014-02-13 18:24:57 -0500

    Removed tuple constructor tests

--- test/mat3alg_test.ml
+++ test/mat3alg_test.ml
@@ -30,29 +30,9 @@ let test_mat _ =
   assert_equal 0.32 (mat_32 a);
   assert_equal 0.33 (mat_33 a)
 
-let test_tuple_vec _ = 
-  assert_equal (0.1, 0.2, 0.3) (tuple_vec (vec 0.1 0.2 0.3))
-
-let test_tuple_covec _ = 
-  assert_equal (0.1, 0.2, 0.3) (tuple_covec (covec 0.1 0.2 0.3))
-
-let test_tuple_mat _ =
-  assert_equal (0.11, 0.12, 0.13,
-				0.21, 0.22, 0.23,
-				0.31, 0.32, 0.33)
-			   (tuple_mat
-				  (mat 0.11 0.12 0.13
-					   0.21 0.22 0.23
-					   0.31 0.32 0.33))
-
-
 let suite = "Mat3alg Tests" >::: ["test_vec" >:: test_vec;
 								  "test_covec" >:: test_covec;
-								  "test_mat" >:: test_mat;
-								  "test_tuple_vec" >:: test_tuple_vec;
-								  "test_tuple_covec" >:: test_tuple_covec;
-								  "test_tuple_mat" >:: test_tuple_mat]
-														 
+								  "test_mat" >:: test_mat]
 
 let _ =
   run_test_tt_main suite
